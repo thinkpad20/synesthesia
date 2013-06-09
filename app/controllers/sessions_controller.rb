0c1fe9ed2d8d0d757e02c6d7996f90ab7973e7a3
thinkpad20 <ithinkican@gmail.com>
2013-06-09 12:49:45 -0500

    can now log in with either username or email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    puts "++++++++++CREATING A SESSION"
 
     if ( !params[:email].present? or !params[:password].present? )
        return redirect_to home_url, notice: 'You are missing an email or password'
      end
 
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email]) if User.find_by_email(params[:email]).present?
+    @user ||= User.find_by_username(params[:email])
+    
+    if not @user.present?
+      puts "Failed when searching for username"
+      redirect_to home_url, notice: "That username/email does not exist."
+      return
+    end
 
-    if @user.present? && @user.authenticate(params[:password])
+    auth = @user.authenticate(params[:password])
+    if auth
       session[:user_id] = @user.id
       redirect_to home_url, notice: 'Signed in successfully'
     else
-      redirect_to home_url, notice: "We couldn't log you in, sure you had the right credentials?"
+      redirect_to home_url, notice: "Incorrect password for that username/email."
     end
   end
 
